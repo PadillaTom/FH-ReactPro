b9c8bd3f0f75c236d2723353bdc2700329a4c1a4
Tomas Padilla <padillatomasagustin@gmail.com>
2023-11-17 21:07:20 +0100

    bases - GSAP added to H2, en H1 todo deberia "saltar"

--- bases/src/bases/CounterEffect.tsx
+++ bases/src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { gsap } from "gsap";
 
 const MAXIMUM_COUNT = 10;
 
@@ -16,11 +17,27 @@ const CounterEffect = () => {
 			"%cSe llego al valor MAXIMO",
 			"color: crimson; background-color: white"
 		);
+
+		gsap
+			.to("h2", {
+				y: -4,
+				duration: 0.1,
+				ease: "ease.out",
+			})
+			.then(() => {
+				gsap.to("h2", {
+					y: 0,
+					duration: 0.3,
+					ease: "bounce.out",
+				});
+			});
 	}, [counter]);
 
 	return (
 		<React.Fragment>
-			<h1>CounterEffect: {counter}</h1>
+			<h1>CounterEffect:</h1>
+			<h2>{counter}</h2>
+
 			<button onClick={handleClick}>+1</button>
 		</React.Fragment>
 	);
